afaceb0e7da4146c30dea5e1fbcdbcf7adb9cb71
avishayhajbi <avishayhajbi@gmail.com>
2014-02-24 03:01:13 +0200

    broad cask finished and calculate time has been successful BUT the notification did now show yet

---
+++ task2do/src/main/java/com/oa/task2do/ReminderBroadCastReceiver.java
@@ -0,0 +1,34 @@
+package com.oa.task2do;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by joe on 24/11/13.
+ */
+public class ReminderBroadCastReceiver extends BroadcastReceiver {
+
+
+    public void	onReceive(Context context,	Intent intent)	{
+//                                System.out.println("BroadcastReceiver: onRecive()");
+
+
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Get taskId & message
+        int taskId = intent.getIntExtra("taskId", 0);
+        String notificationText = intent.getStringExtra("taskMessage");
+
+        Intent myIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, myIntent, 0);
+
+        Notification notification = new Notification(R.drawable.ic_launcher, "New scheduled task", System.currentTimeMillis());
+        notification.setLatestEventInfo( context,"Task2Do", notificationText, pendingIntent);
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(null, taskId, notification); //0 is id
+    }
+}
